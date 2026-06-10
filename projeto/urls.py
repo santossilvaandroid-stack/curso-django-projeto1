b7aaed427337f9a422fd53b403e596eecde5b0bb
Michael Santos <santossilvaandroid@gmail.com>
2026-06-10 14:39:40 -0300

    Criado o apps no Django

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("HOME")
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
 ]
