e0da27ae290b8555c1e0e24062daed63524f62da
Michael Santos <santossilvaandroid@gmail.com>
2026-06-10 15:26:35 -0300

    Movendo código para o app recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,23 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse("HOME")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', include('recipes.urls')),
 ]
