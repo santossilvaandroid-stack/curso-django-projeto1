6a57815659db4cb079525b5071faeb7f30ddbff4
Michael Santos <santossilvaandroid@gmail.com>
2026-06-17 20:24:41 -0300

    Adicionando Recipes na admin do Django

--- projeto/settings.py
+++ projeto/settings.py
@@ -122,3 +122,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- recipes/models.py
+++ recipes/models.py
@@ -31,6 +31,10 @@ class Recipe(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
 
+
+    def __str__(self):
+            return self.title
+
 # EDITED
 # title description slug
 # preparation_time preparation_time_unit
