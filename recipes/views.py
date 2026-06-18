d62f9b271e1187c18fbf1788389226e828e44cf1
Michael Santos <santossilvaandroid@gmail.com>
2026-06-18 20:41:37 -0300

    Usando o model Recipe real na view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
+
+from .models import Recipe
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
 
 
 def recipe(request, id):
@@ -14,3 +24,16 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
+
+def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
