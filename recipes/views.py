528a9c48ad8c0f5ef7d901a5151d29eb8bd4d1ab
Michael Santos <santossilvaandroid@gmail.com>
2026-06-19 20:53:23 -0300

    Filtrando is_published

--- recipes/views.py
+++ recipes/views.py
@@ -7,17 +7,20 @@ from .models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+        recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+        return render(request, 'recipes/pages/home.html', context={
+            'recipes': recipes,
+        })
 
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
