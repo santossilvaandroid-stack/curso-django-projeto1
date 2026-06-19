0d899aa652bee56d64f4ab4e48c4f8a4559898fc
Michael Santos <santossilvaandroid@gmail.com>
2026-06-19 20:25:55 -0300

    Corrigindo: Usando o model Recipe real na view home

--- recipes/views.py
+++ recipes/views.py
@@ -17,6 +17,9 @@ def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id
     ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
 
 
 def recipe(request, id):
@@ -24,16 +27,3 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
-
-def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
-
-
-
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
-    ).order_by('-id')
