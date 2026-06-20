d545ac0fc23ac5d45939923d7416dd5f6248ad32
Michael Santos <santossilvaandroid@gmail.com>
2026-06-19 21:07:25 -0300

    Corrigindo category com blank True e default None

--- recipes/views.py
+++ recipes/views.py
@@ -7,16 +7,22 @@ from .models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+        recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+        return render(request, 'recipes/pages/home.html', context={
+            'recipes': recipes,
+        })
 
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
 
 
 def recipe(request, id):
@@ -24,16 +30,3 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
-
-def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
-
-
-
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
-    ).order_by('-id')
