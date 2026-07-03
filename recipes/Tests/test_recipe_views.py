c3e102200f7a8ac11e71f1327ce2910075c810fc
Michael Santos <santossilvaandroid@gmail.com>
2026-07-02 21:10:49 -0300

    Testando 404 Not Found quando receitas não existem

--- recipes/Tests/test_recipe_views.py
+++ recipes/Tests/test_recipe_views.py
@@ -37,3 +37,24 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here 🥲</h1>',
             response.content.decode('utf-8')
         )
+
+    
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertIs(view.func, views.category)
+
+
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
