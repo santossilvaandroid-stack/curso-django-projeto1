2688807f109ee9e17dcd893bc84a5cd53ed57ce1
Michael Santos <santossilvaandroid@gmail.com>
2026-07-05 20:44:11 -0300

    Testando se category e detail carregam a receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -53,6 +53,18 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+        
+
     def test_recipe_home_template_loads_recipes(self):
         
         self.make_recipe()
@@ -71,6 +83,26 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
+
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'This is a detail page - It load one recipe'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id': 1
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
 
 
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
