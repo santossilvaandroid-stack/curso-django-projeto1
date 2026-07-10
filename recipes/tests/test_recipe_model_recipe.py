5da7e661cbbb3c0aed81f309cb40541f5c0f314c
Michael Santos <santossilvaandroid@gmail.com>
2026-07-09 21:10:56 -0300

    Criando testes parametrizados com parameterized

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,6 @@
 from .test_recipe_base import RecipeTestBase # type: ignore
+from parameterized import parameterized
+from .test_recipe_base import RecipeTestBase # type: ignore
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -6,6 +8,13 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError): # type: ignore
+            self.recipe.full_clean()
