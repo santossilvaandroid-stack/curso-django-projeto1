c388f92a38102d6dfd0afe9269c1bb414341444a
Michael Santos <santossilvaandroid@gmail.com>
2026-08-31 21:24:35 -0300

    add_attr

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -11,6 +15,7 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
 
     password = forms.CharField(
         required=True,
@@ -32,7 +37,7 @@ class RegisterForm(forms.ModelForm):
             'placeholder': 'Repeat your password'
         })
     )
-    
+
     class Meta:
         model = User
         fields = [
