bc0f9b45faf8540b31fee76a8a85916eeed82476
Michael Santos <santossilvaandroid@gmail.com>
2026-09-01 21:27:39 -0300

    is_valid and save

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import redirect, render
 from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
@@ -18,5 +19,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
