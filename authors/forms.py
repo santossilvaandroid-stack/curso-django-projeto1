ce1ffe6ffdf2fe40e1d10690a727de1b251f8858
Michael Santos <santossilvaandroid@gmail.com>
2026-09-10 21:46:24 -0300

    Testando campos vazios

--- authors/forms.py
+++ authors/forms.py
@@ -33,6 +33,20 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Type your password')
         add_placeholder(self.fields['password2'], 'Repeat your password')
 
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.',
+    )
+
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
@@ -43,9 +57,11 @@ class RegisterForm(forms.ModelForm):
         label='Password'
     )
     password2 = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
-        label='Password2'
+        label='Password2',
+        error_messages={
+            'required': 'Please, repeat your password'
+        }
     )
 
     class Meta:
@@ -60,12 +76,6 @@ class RegisterForm(forms.ModelForm):
         
         labels = {
             'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-        }
-        help_texts = {
-            'email': 'The e-mail must be valid.',
         }
         error_messages = {
             'username': {
