0580d38a073de18ec54a4e9e69768da39ee2823e
Michael Santos <santossilvaandroid@gmail.com>
2026-09-16 21:24:14 -0300

    User e-mail must be unique

--- authors/forms.py
+++ authors/forms.py
@@ -87,7 +87,17 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-        
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+
+        return email
 
 
     def clean(self):
