4eeeb2ab05221242c6166337d42e10af8b34f2e1
Michael Santos <santossilvaandroid@gmail.com>
2026-09-21 21:25:16 -0300

    Autenticando o usuário

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from .forms import LoginForm, RegisterForm
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register_view(request):
@@ -40,4 +41,24 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
